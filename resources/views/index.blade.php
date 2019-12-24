bb5a1e3ec9e3c87404c2b0cc00307fa563d1ff8f
athirahzuhaini <59073913+athirahzuhaini@users.noreply.github.com>
2019-12-24 17:15:16 +0800

    Add files via upload

---
+++ resources/views/index.blade.php
@@ -0,0 +1,350 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Resume Athirah Zuhaini</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet">
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/resume.min.css" rel="stylesheet">
+  <link href="img/icon.png" rel='shortcut icon' type='image/png'/>
+
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+
+</head>
+
+<body id="page-top">
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+    <a class="navbar-brand js-scroll-trigger" href="#page-top">
+      <span class="d-block d-lg-none">Che Wan Nurfatin Athirah</span>
+      <span class="d-none d-lg-block">
+        <img class="img-fluid img-profile rounded mx-auto mb-2" src="img/profile.jpg" alt="">
+      </span>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#about">About  <img src="img/about.png" alt=""></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#experience">Experience <img src="img/briefcase.png" alt=""></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#education">Education <img src="img/mortarboard.png" alt=""></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#skills">Skills<img src="img/skills.png" alt=""></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#awards">Awards<img src="img/awards.png" alt=""></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#gallery">Gallery   <img src="img/image.png" alt=""></a>
+        </li>
+        </ul>
+    </div>
+  </nav>
+
+  <div class="container-fluid p-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
+      <div class="w-100">
+        <h1 class="mb-0">Athirah
+          <span class="text-primary">Zuhaini</span>
+        </h1>
+        <div class="subheading mb-5">19 Jalan 4/27B Desa Setapak 53300 Kuala Lumpur · 013-9165425 ·
+          <a href="mailto:name@email.com">tyrazuhaini@gmail.com</a>
+        </div>
+        <p class="lead mb-5">A highly motivated graduate in Information Systems Management from Universiti Teknologi MARA
+                              seeking for career opportunities in the related field. A performance-oriented 
+                              individual who can help your company to accomplish its goal and objectives thus make a difference.</p>
+        
+          <div class="social-icons">
+          <a href="https://www.linkedin.com/in/athirahzuhaini/" target="_blank">
+            <i class="fab fa-linkedin-in"></i>
+          </a>
+          <a href="https://github.com/athirahzuhaini/" target="_blank">
+            <i class="fab fa-github"></i>
+          </a> 
+          <a href="https://wa.me/60139165425" target="_blank">
+            <i class="fab fa-whatsapp"></i>
+          </a>   
+          <a href="pdf/Che Wan Nurfatin Athirah_Resume_0139165425.pdf" download="Resume Che Wan Nurfatin Athirah">
+            <i class="fas fa-file-pdf"></i>
+          </a>       
+      </div>
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex justify-content-center" id="experience">
+      <div class="w-100">
+        <h2 class="mb-5">Experience</h2>
+
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">GRADUATE EXECUTIVE TRAINEE</h3>
+            <div class="subheading mb-3">Permodalan Nasional Berhad</div>
+            <img class="img-fluid" src="img/pnb.png" alt="" width="200" height="89"><br><br>
+            <p>Assist webmaster on managing PNB Group website<br>
+                Prepare online survey forms for PNB Group using WordPress plugin<br>
+                Involve updating Departmental Business Continuity Plan (DBCP)<br>
+                Develop editable pdf documents pertaining ASNB online forms for ASNB website<br>
+                To maintain log and list of PNB corporate email using Google Sheets and Outlook Office 365<br>
+                Performing filing work<br>
+                Involve in PNB Corporate Summit 2019 event and managed Slido for the moderators</p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">June 2019 - Present</span>
+          </div>
+        </div>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">INTERN</h3>
+            <div class="subheading mb-3">PETRONAS Gas Berhad Kuantan</div>
+            <img class="img-fluid" src="img/pgb.png" alt="" width="120" height="124"><br><br>
+            <p>Performed general administrative duties such as preparing minutes of meeting and compliance audit forms<br>
+                Assisted Executive of Administration in daily duties<br>
+                Updated employees' travel claims in SAP HR System<br>
+                Assisted in conducting audits<br>
+                Edited videos using Wondershare Filmora software<br>
+                Improvised database management system for Vehicle Maintenance using Microsoft Excel VBA<br>
+                Assisted in Kuantan RO Family Day 2018 and PETRONAS Public Awareness event<br>
+                Assisted in conducting PETRONAS Management System Review (MSR) meeting</p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">August 2018 - December 2018</span>
+          </div>
+        </div>
+      </div>
+
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="education">
+      <div class="w-100">
+        <h2 class="mb-5">Education</h2>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div id="accordion">
+
+          <div class="card">
+            <div class="card-header">
+              <a class="card-link" data-toggle="collapse" href="#collapseOne">
+                <div class="subheading mb-3">Bachelor of Information Science (Hons) Information Systems Management (2015-2019)</div>
+              </a>
+            </div>
+            <div id="collapseOne" class="collapse show" data-parent="#accordion">
+              <div class="card-body">
+              <img class="img-fluid" src="img/uitm.png" alt="" width="200" height="84"><br><br> 
+                <p>Graduated with a 3.45 CGPA<br>
+                    Member, Association of Information Management Systems<br>
+                    Committee member, Legal & Ethical Aspects of Information Systems subject<br>
+                    Team Leader, System Analysis in Information Management final project</p>
+              </div>
+              
+            </div>
+          </div>
+
+
+
+
+
+          <div class="card">
+            <div class="card-header">
+              <a class="collapsed card-link" data-toggle="collapse" href="#collapseTwo">
+                <div class="subheading mb-3">Diploma in Teaching of English as A Second Language (2011-2014)</div>
+              </a>
+            </div>
+            <div id="collapseTwo" class="collapse" data-parent="#accordion">
+              <div class="card-body">
+              <img class="img-fluid" src="img/kptm.png" alt="" width="200" height="98"><br><br>  
+                <p>Graduated with a 3.39 CGPA<br>
+                    Facilitator, KPTM 2012 New Students Intake<br>
+                    Crew, Secretariat of MPP KPTM KL</p>
+              </div>
+            </div>
+          </div>
+  
+           <div class="card">
+            <div class="card-header">
+              <a class="collapsed card-link" data-toggle="collapse" href="#collapseThree">
+                <div class="subheading mb-3">Secondary Education (2006-2010)</div>
+              </a>
+            </div>
+            <div id="collapseThree" class="collapse" data-parent="#accordion">
+              <div class="card-body">
+              <img class="img-fluid" src="img/smkbr.png" alt="" width="127" height="113"><br><br>  
+                <p>Passed<br>
+                  Exco in Prefect Board<br>
+                    Vice President of Computer Club<br>
+                    Vice Treasurer of Chess Club<br>
+                    Treasurer of Computer Club</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="skills">
+      <div class="w-100">
+        <h2 class="mb-5">Skills</h2>
+
+        <div class="subheading mb-3">Professional Skills</div>
+        <ul class="list-inline dev-icons">
+          <li class="list-inline-item">
+            <i class="fas fa-file-word"></i>
+          </li>
+          <li class="list-inline-item">
+           <i class="fas fa-file-powerpoint"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fas fa-file-excel"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fab fa-html5"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fab fa-css3-alt"></i>
+          </li>
+           <li class="list-inline-item">
+            <i class="fab fa-js-square"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fab fa-bootstrap"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fab fa-wordpress"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fab fa-php"></i>
+          </li>
+         </ul>
+
+        <div class="subheading mb-3">Personal Skills</div>
+        <ul class="fa-ul mb-0">
+          <li>
+            <i class="fa-li fa fa-check"></i>
+            Good communication skills</li>
+          <li>
+            <i class="fa-li fa fa-check"></i>
+            Reliable &amp; professional</li>
+          <li>
+            <i class="fa-li fa fa-check"></i>
+            Organized &amp; good time management skills</li>
+          <li>
+            <i class="fa-li fa fa-check"></i>
+            Proven to be able to work independently or as part of a team</li>
+        </ul>
+      </div>
+    </section>
+
+
+    <hr class="m-0">
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="awards">
+      <div class="w-100">
+        <h2 class="mb-5">Awards &amp; Certifications</h2>
+        <div class="subheading mb-3">Awards</div>
+        <ul class="fa-ul mb-0">
+          <li>
+            <i class="fa-li fa fa-trophy text-warning"></i>
+            Dean Awards - December 2018, Bachelor of Information Science (Hons) Information Systems Management</li>
+          <li>
+            <i class="fa-li fa fa-trophy text-warning"></i>
+            Dean Awards -July 2013, Diploma in Teaching of English as A Second Language (TESL)</li>
+          <li>
+            <i class="fa-li fa fa-trophy text-warning"></i>
+            Dean Awards -January 2013, Diploma in Teaching of English as A Second Language (TESL)</li>
+          </ul><br>
+        <div class="subheading mb-3">Certifications</div>
+        <ul class="fa-ul mb-0">
+         <li>
+            <i class="fa-li fa fa-trophy text-warning"></i>
+           EQ in Sales & Effective Closing  - Issued by : AR Resources</li>
+          <li>
+            <i class="fa-li fa fa-trophy text-warning"></i>
+            Computerized Unit Trust Exam - Issued by : Federation of Investment Managers Malaysia</li>
+          <li>
+          </ul>
+      </div>
+    </section>
+
+
+
+
+
+    
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="gallery">
+      <div class="w-100">
+        <h2 class="mb-5">Gallery</h2>
+        
+        <div class="bekas-all">
+<div class="bekas">
+  <img class="img-fluid" src="img/2018pgb.jpeg" alt=""width="600" height="400">
+   <span class="title">2018 Moments</span><br>
+
+  <img class="img-fluid"src="img/2018pgb2.jpg" alt=""width="600" height="400">
+   <span class="text">Top : Public Awareness event for Local Authorities<br>
+   Bottom : Site Visit for ERE </span>
+</div>
+<div class="bekas">
+  <img class="img-fluid"src="img/2019pnb.jpg" alt=""width="600" height="400">
+   <span class="title">2019 Moments</span><br>
+
+  <img class="img-fluid"src="img/2019pnb2.jpg" alt=""width="600" height="400">
+   <span class="text">Top : With PNB's new CEO<br>
+   Bottom : Farewell event for former PNB's CEO</span>
+</div>
+         </div>
+    </section>
+
+ 
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script type="text/javascript" src="js/addons-pro/timeline.js"></script>
+
+
+  <!-- Custom scripts for this template -->
+  <script src="js/resume.min.js"></script>
+   
+  
+
+</body>
+
+</html>
